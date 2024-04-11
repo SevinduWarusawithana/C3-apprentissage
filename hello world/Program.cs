c51f3c6c30b6bccff36cfaf39f0fc649b35fa804
SeviRanWaru <149356832+SeviRanWaru@users.noreply.github.com>
2024-04-11 17:26:35 +0530

    Add project files.

---
+++ hello world/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int age = 21;
+            //Console.WriteLine(age);
+
+            string name = "sevindu";
+
+            Console.Write("My name is ");
+            Console.Write(name);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
